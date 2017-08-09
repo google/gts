327e09c3590b823c31b772ec69c7a4a540850969
Ali Ijaz Sheikh <ofrobots@google.com>
2017-08-08 21:24:06 -0700

    feat: add format.ts
    Use clang-format API to invoke rather than spawning directly.
    
    Add ability to run check as well as fix.

---
+++ src/format.ts
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {Options} from './cli';
+import {createProgram} from './lint';
+
+const clangFormat = require('clang-format');
+
+const baseArgs =
+    ['-style', '{Language: JavaScript, BasedOnStyle: Google, ColumnLimit: 80}'];
+
+/**
+ * Run tslint fix and clang fix with the default configuration
+ * @param fix whether to automatically fix the format
+ * @param options
+ */
+export function format(fix: boolean, options: Options): Promise<boolean> {
+  const program = createProgram(options);
+  const srcFiles = program.getSourceFiles()
+                       .map(sourceFile => sourceFile.fileName)
+                       .filter(f => !f.endsWith('.d.ts'));
+
+  return fix ? fixFormat(srcFiles) : checkFormat(srcFiles);
+}
+
+/**
+ * Runs clang-format to automatically fix the format of supplied files.
+ *
+ * @param srcFiles list of source files
+ */
+function fixFormat(srcFiles: string[]): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    const args = baseArgs.concat(['-i'], srcFiles);
+    clangFormat.spawnClangFormat(args, (err?: Error) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(true);
+      }
+    }, 'inherit');
+  });
+}
+
+/**
+ * Runs clang-format on the list of files and checks whether they are formatted
+ * correctly. Returns true if all files are formatted correctly.
+ *
+ * @param srcFiles list of source files
+ */
+function checkFormat(srcFiles: string[]): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    let output = '';
+    const args = baseArgs.concat(['-output-replacements-xml'], srcFiles);
+    console.log('spawning clang for formatting');
+    const out = clangFormat
+                    .spawnClangFormat(
+                        args,
+                        (err?: Error) => {
+                          if (err) {
+                            reject(err);
+                          }
+                        },
+                        ['ignore', 'pipe', process.stderr])
+                    .stdout;
+    out.setEncoding('utf8');
+    out.on('data', (data: Buffer) => {
+      output += data;
+    });
+    out.on('end', () => {
+      resolve(output.indexOf('<replacement ') === -1 ? true : false);
+    });
+  });
+}
