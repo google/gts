84dc19f0d861d11c9cd7e870636a58d1a4aeb672
Jinwoo Lee <jinwoo@google.com>
2018-02-05 15:15:56 -0800

    feat: use typescript 2.7.1 and esModuleInterop
    [BREAKING CHANGE]
    
    This PR is both for the generated `package.json` and `gts` itself.
    
    TypeScript 2.7.1 added `--esModuleInterop` for better ES module
    support. And for that, we need to turn on `allowSyntheticDefaultImports`
    too.
    
    Also `tslint` must be upgraded to 5.9.1 for it to compile with
    `esModuleInterop`.

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -16,9 +16,9 @@
 
 import {test, Test} from 'ava';
 import * as fs from 'fs';
-import * as makeDir from 'make-dir';
+import makeDir from 'make-dir';
 import * as path from 'path';
-import * as pify from 'pify';
+import pify from 'pify';
 import * as tmp from 'tmp';
 
 const writeFilep = pify(fs.writeFile);
