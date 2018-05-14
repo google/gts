07ea10ca15ab11044ac15a42371d13655ae3d6b0
Ali Ijaz Sheikh <ofrobots@google.com>
2018-05-14 12:17:38 -0700

    fix tests

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -16,9 +16,9 @@
 
 import {test, Test} from 'ava';
 import * as fs from 'fs';
-import makeDir from 'make-dir';
+import * as makeDir from 'make-dir';
 import * as path from 'path';
-import pify from 'pify';
+import * as pify from 'pify';
 import * as tmp from 'tmp';
 
 const writeFilep = pify(fs.writeFile);
