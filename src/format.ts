0fa2476bc4ae52d97aec41a36d3393e504853c24
Tammy Lee <ltammy@google.com>
2018-07-09 11:18:46 -0700

    Able to separate string into multiple diffs and print them

--- src/format.ts
+++ src/format.ts
@@ -19,6 +19,7 @@ import {Options} from './cli';
 import {createProgram} from './lint';
 import { promisify } from 'util';
 import { start } from 'repl';
+import { forEachChild } from 'typescript';
 
 // Exported for testing purposes.
 export const clangFormat = require('clang-format');
@@ -153,28 +154,15 @@ async function findFormatErrorLines(output: string, options: Options){
     const read = promisify(fs.readFile);
     const argNum = 3;
     const file = process.argv[argNum + i - 1];
-
     const data = await read(file, 'utf8');
 
     let fixed = performFixes(data, errOffset, errLength, replacements);
     let diff = jsdiff.structuredPatch('oldFile', 'newFile', data, fixed, 'hi1', 'hi2', 
-        [{oldStart: 1, oldLines: 2, newStart: 1, newLines: 2,
-      lines: [' line2', ' line3', '-line3', '+line3', '\\ No newline at end of file']}]);
- 
-    console.log(diff);
-    jsdiff.applyPatches(diff);
-    /*diff.forEach(function(part:any){
-      // green for additions, red for deletions
-      // grey for common parts
-      if(part.added){
-        process.stderr.write(chalk.bgGreen(part.value));
-      }else if(part.removed){
-        process.stderr.write(chalk.bgRed(part.value));
-      }else{
-        process.stderr.write(chalk.bgGreen(part.value));
-      }
-    });*/
+    {context: 1});
+
     
+    jsdiff.applyPatch('data', diff);
+    printDiffs(diff.hunks);
   }
 }
 
@@ -194,68 +182,18 @@ function performFixes(data: string, errOffset: string[], errLength: string[], re
     return replaced.join('');
 }
 
-
-    /*lines = data.split('\n');
-    let linesOfCharCount: number[] = []
-    linesOfCharCount[0] = 0;
-    for(let i = 1; i < lines.length; i++){
-      linesOfCharCount[i] = linesOfCharCount[i - 1] + lines[i - 1].length + 1;
-    }
-
-    for(let i = 0; i < errOffset.length;){
-      let startLine = findErrLine(file, +errOffset[i], linesOfCharCount);
-      let endLine = findErrLine(file, +errOffset[i] + +errLength[i], linesOfCharCount);
-      if(startLine === endLine){
-        printFormatErrLines(lines[startLine], +errOffset[i] - linesOfCharCount[startLine], 
-            +errLength[i], startLine, file, options);
+function printDiffs(diffs: string[]){
+  diffs.forEach(function(diff:any){
+    // green for additions, red for deletions
+    // grey for common parts
+    diff.lines.forEach(function(line:any){
+      if(line[0] === '+'){
+        process.stderr.write(chalk.green.bold(line) + '\n');
+      }else if(line[0] === '-'){
+        process.stderr.write(chalk.red.bold(line) + '\n');
       }else{
-        
-        printFormatErrLines(lines[startLine] + '\xB6' , +errOffset[i] - linesOfCharCount[startLine], 
-            +errOffset[i] + 1, startLine, file, options);
-        
-        if(startLine + 1 < endLine){
-          console.log(file + ":" + (startLine + 1) + "-" + (endLine - 1) + "   remove lines");
-        }
+        process.stderr.write(chalk.black(line) + '\n');
       }
-      i++;
-      
-    }
-  }
+    });
+  });
 }
-
-function findErrLine(file: string, offset: number, linesOfCharCount: number[]): number{
-  let left = 0;
-  let right = linesOfCharCount.length - 1;
-  for(let i = 1; i < linesOfCharCount.length; i++){
-    if(linesOfCharCount[i] >= offset){
-      return i - 1;
-    }
-  }
-  return 0;
-}
-*/
-/**
- * Prints the line with formatting issues.
- * 
- * @param line string in file with formatting issue
- * @param pos position of the start of formatting issue
- * @param length the length of the formatting issue
- * @param lineNumber 
- * @param file the file where formatting issue is
- */
-function printFormatErrLines(line: string, pos: number, length: number, 
-    lineNumber: number, file: string, options: Options){
-  const header = file + ':' + lineNumber;
-  const spacing = header.length + 3;
-  process.stdout.write(header);
-  console.log(' '.repeat(3) + line);
-  process.stdout.write(' '.repeat(spacing));
-  for(let i = 0; i < line.length; i++){
-    if(i >= pos && i < (+pos + +length)){
-      process.stdout.write('^');
-    }else{
-      process.stdout.write('-');
-    }
-  }
-  console.log();
-    }
