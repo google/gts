53d4e1c7ee3e688098bd1345f4dd0bb938578e85
Justin Beckwith <justin.beckwith@gmail.com>
2018-12-10 16:14:48 -0800

    simplify format check

--- bin/format-check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-output=$(prettier --list-different src/*.ts test/*.ts)
-len=${#output}
-if [[ ${len} != 0 ]] ; then
-  echo "Format errors found. Run 'npm run format' to fix."
-  exit 1
-fi
