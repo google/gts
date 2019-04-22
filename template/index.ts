a7652a613d845962868ff505c0ae5eb8fa07d408
Unknown <pliski@gmail.com>
2019-04-22 17:12:12 +0200

    docs : add an example in template

--- template/index.ts
+++ template/index.ts
@@ -7,6 +7,8 @@ const trailing = 'Semicolon'
             const why = 'am I tabbed?';
 
 export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
+    //function on one line
+    if(!andThose.length) {return false;}
     console.log(withThis);
     console.log(andThat);
     console.dir(andThose);
