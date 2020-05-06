dea17d604296399a5af19883936be420799e93a0
Alexander Fenster <fenster@google.com>
2020-05-05 17:26:08 -0700

    fix: misleading error message
    If `tsconfig.json` cannot be parsed, don't say it's not found.

--- src/util.ts
+++ src/util.ts
@@ -76,6 +76,10 @@ async function getBase(
   readFiles.add(filePath);
   try {
     const json = await customReadFilep(filePath, 'utf8');
+  } catch (err) {
+    throw new Error(`${filePath} not found`);
+  }
+  try {
     let contents = JSON.parse(json);
 
     if (contents.extends) {
@@ -91,7 +95,7 @@ async function getBase(
 
     return contents;
   } catch (err) {
-    throw new Error(`${filePath} Not Found`);
+    throw new Error(`${filePath} cannot be parsed`);
   }
 }
 
